bcc1634d1f324889fc9cd5a9aa3fd53b968e10fe
aricadawn <119612721+aricadawn@users.noreply.github.com>
2023-04-06 13:12:26 -0400

    Add files via upload

---
+++ STIG_NQE.py
@@ -0,0 +1,57 @@
+import csv
+import requests
+import json
+import sys
+import filters
+
+# import apiVb.py file where api url and token are stored locally
+sys.path.insert(0, '/Users/aricabeckstead/')
+import apiVb #type:ignore
+# EXAMPLE:
+# API_URL = "https://fwd.app/api/users/current/nqe/changes?action=addQuery&path=/{FOLDER}/{QUERY-NAME}"
+# TOKEN_FNSAAS = ('ACCESS-KEY', 'SECRET-KEY')
+
+API_URL = apiVb.API_URL
+TOKEN = apiVb.TOKEN_FNSAAS
+
+# JSON formatted NQE 
+nqe = '''
+/**\n * @intent {}\n\n * @description {}\n\n * Version//Revision: V2R4\n * Vuln ID: {}\n * Severity: {}\n * Group Title: {}\n * Rule ID: {}\n * Rule_Version: {}\n */\n\n{}\n\n/* \nCheck Content: {}\n*/\n\n/* \nFix Text: {}\n*/\n\nstigData =\n  {{ os: OS.IOS_XE,\n    vulnId: "{}",\n    groupTitle: "{}",\n    ruleId: "{}",\n    severity: "{}",\n    ruleVersion: "{}",\n    legacyVulns: "[{}]"\n  }}; \n{}
+'''
+
+NQE_txt = 'queries.txt'
+STIG_csv = 'Cisco IOS XE Router RTR V2R6.csv'
+creat_dir = 'https://fwd.app/api/users/current/nqe/changes?action=addDir&path=/Arica/{}/'.format(STIG_csv.strip('.csv'))
+
+def STIG_NQE(NQE_txt, STIG_csv):
+   '''
+   1. file_out stores queries in txt file - used for testing.
+   2. file_in is an export of STIG in csv format.
+   3. Iterates over file_in to retrieve relevant stig information and create NQE.
+   4. Creates API POST to create NQE for each STIG.
+   '''
+   r = requests.post(creat_dir, auth = TOKEN)
+   with open(NQE_txt, 'w') as file_out:
+      with open(STIG_csv, 'r') as file_in:
+         reader = csv.DictReader(file_in)
+         for row in reader:
+               a = row['Vuln ID']
+               b = row['Severity']
+               c = row['Group Title']
+               d = row['Rule ID']
+               e = row['STIG ID']
+               f = row['Legacy']
+               g = row['Rule Title']
+               h = row['Discussion']
+               i = row['Fix Text']
+               j = row['Check Content']
+               k, l = filters.dictionary(filters.pattern(j))
+               sourceCode = nqe.format(g,h,a,b,c,d,e,k,j,i,a,c,d,b,e,f,l)
+               payload = {'queryType': 'QUERY', 'sourceCode': sourceCode}
+               r = requests.post(API_URL.format(STIG_csv.strip('.csv'), e), json=payload, auth=TOKEN)
+               # file_out.write(json.dumps(payload))
+               file_out.write(sourceCode)
+               
+
+if __name__ == '__main__':
+   STIG_NQE(NQE_txt, STIG_csv)
---
+++ filters.py
@@ -0,0 +1,160 @@
+import re
+
+def pattern(check_content): 
+    """
+    Filters Check Content portion of STIG to collect configuration and create a list of pattern variables. 
+    """
+    mode = False
+    lines = []
+    for line in check_content.splitlines():
+      if not mode:
+        if line.startswith(('Review', 'Step', 'Verify', 'Cisco router')) or line.endswith(('example below:', 'example below.')):
+          mode = True
+      elif line.startswith('If'):
+        mode = False
+      else:
+        lines.append(line)
+
+    filters = [r'Example', r'Note', r'Step', r'example', r'proceed', r'below', r'Verify', r'Review', r'Altern', r'!', r'https://', r'IF']
+    for word in filters:
+        for item in lines:
+            if re.search(word, item):
+                lines.remove(item)
+            elif u'\u2026' in item:
+                lines.remove(item)
+                
+    interfaceCnt = 1
+    keyCnt = 1
+    ipCnt = 1
+    for index, item in enumerate(lines):
+       if 'interface' in item:
+          lines[index] = '''interface {}'''.format(interfaceCnt)
+          interfaceCnt += 1
+       elif 'key-string' in item:
+          lines[index] += '{{key{}: string}}'.format(keyCnt)
+          keyCnt += 1
+       elif 'ip address' in item:
+          lines[index] = ''' ip address {ip: ipv4Address}'''
+          ipCnt += 1
+       elif 'description' in item:
+          lines[index] = ''' description {desc: string}'''
+    return lines
+
+def dictionary(pattern):
+    """
+    1. Creates a dictionary of pattern variables by filtering through list created in pattern function.
+    2. Returns pattern variables and formatted query as strings 
+    """
+    query = [
+       'foreach device in network.devices',
+       'where device.platform.os == stigData.os',
+    ]
+    intQuery = '''
+interfaceViolation(device) = 
+foreach interface in patternMatches(device.files.config, `interface {name:string}`)
+let lines = (foreach child in interface.line.children select child)
+where'''
+    select1 = '''
+select {
+  violation: isPresent(device.name),
+  device: device.name,
+  deviceModel: device.platform.model,
+  mgmtIP: device.platform.managementIps,
+  Severity: stigData.severity,
+  Group_Title: stigData.groupTitle,
+  Vuln_ID: stigData.vulnId,
+  Rule_ID: stigData.ruleId,
+  Rule_Version: stigData.ruleVersion
+}'''
+    select2 = '''
+select {
+  violation: isPresent(device.name),
+  device: device.name,
+  interface: (foreach int in intViolation select int),
+  deviceModel: device.platform.model,
+  mgmtIP: device.platform.managementIps,
+  Severity: stigData.severity,
+  Group_Title: stigData.groupTitle,
+  Vuln_ID: stigData.vulnId,
+  Rule_ID: stigData.ruleId,
+  Rule_Version: stigData.ruleVersion
+}'''
+    dictionary = {}
+    def setname(d, n):
+        if 'interface' in n:
+           d[n] = []
+        else:
+           d[n] = [n]
+
+    for item in [z for z in pattern if len(z) > 1]:
+        """
+        1. Reformats items returned in pattern to match configuration syntax
+        2. Creates dictionary of pattern variables 
+        """
+        tup = [(r' [nxyz]{2,}', ' '), (r'^deny', ' deny'), (r'^permit', ' permit'), (r'^ router', 'router'), (r'^neighbor', ' neighbor'), (r'^remark', ' remark'), (r'^or$', ' '), ('interface interface', 'interface')]
+        for i in tup:
+            item = re.sub(i[0], i[1], item)
+        try:
+            if len(item) > 0:
+                if item[0] != ' ':
+                    key = item
+                    setname(dictionary, key)
+                else:
+                    value = item
+                    dictionary[key].append(value)
+        except KeyError:
+            continue
+
+    for x,y in list(dictionary.items()):
+       if len(y) > 1:
+          dictionary[' '.join(x.split()[0:2])] = dictionary.pop(x)
+
+    queryLine = ''
+    config = 'pattern = ```\n'
+    intCnt = 0
+    configCnt = 1
+    where = []
+    for n in dictionary:
+        """
+        1. Creates unique query variables for each requirement
+        2. Creates interface function to return a list of violating interfaces
+        3. Formats query to search device configuration for pattern and return violating devices
+        """
+        if len(dictionary[n]) > 1 or 'interface' in n:
+            queryLine += '{} = ```\n'.format(n.replace(' ', '_').replace('-','_'))
+            if 'interface' in n:
+               if intCnt == 0:
+                   intQuery += ' !hasBlockMatch_alpha1(lines, {})'.format(n.replace(' ', '_').replace('-','_'))
+                   query.insert(2, 'let intViolation = interfaceViolation(device)')
+                   intCnt =+ 1
+               elif intCnt > 0:
+                   intQuery += ' && !hasBlockMatch_alpha1(lines, {})'.format(n.replace(' ', '_').replace('-','_'))
+            else:
+               query.append('let config{} = max(blockMatches_alpha1(device.files.config, {}))'.format(configCnt, n.replace(' ', '_').replace('-','_')))
+               configCnt += 1
+            for o in dictionary[n]:
+                if len(o) > 1:
+                    queryLine += '{}\n'.format(o)
+            queryLine += '```; \n'
+        else:
+           config += '{}\n'.format(n)
+    config += '```;\n'
+    if len(config) > 19:
+       queryLine += config
+       query.append('let config = max(blockMatches_alpha1(device.files.config, pattern))')
+       where.append('!isPresent(config)')
+    if configCnt > 1:
+       for i in range(1, configCnt):
+             where.append('!isPresent(config{})'.format(i))    
+    if intCnt > 0:
+       intQuery += '\nselect interface;\n'
+       query.insert(0, intQuery)  
+       where.append('length(intViolation) > 0')  
+       end = select2
+    else:
+       end = select1
+    where = 'where '+' || '.join(i for i in where)
+    query.append(where)
+    query.append(end)
+    query = '\n'.join(i for i in query)
+    return queryLine, query
